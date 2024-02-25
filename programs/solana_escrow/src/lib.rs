6dab10879d9d1167aac8216abe46369ec5832a79
nomadic4life <orbison.dennis@gmail.com>
2024-02-25 00:47:23 -0800

    implemented to create token account for authority of a given mint

--- programs/solana_escrow/src/lib.rs
+++ programs/solana_escrow/src/lib.rs
@@ -1,4 +1,6 @@
 use anchor_lang::prelude::*;
+use anchor_spl::associated_token::AssociatedToken;
+use anchor_spl::token::{Mint, Token, TokenAccount};
 
 declare_id!("HNnEWxRvbw5Kf5oCkgTeLa1BNM4racVQELGrEn4K9GQd");
 
@@ -24,6 +26,17 @@ pub mod solana_escrow {
 
         return Ok(());
     }
+
+    pub fn create_authority_token_account(
+        ctx: Context<InitializeAuthorityTokenAccount>,
+    ) -> Result<()> {
+        msg!(
+            "Created The Authority Token Account: {}",
+            ctx.accounts.token_account.key()
+        );
+
+        return Ok(());
+    }
 }
 
 #[derive(Accounts)]
@@ -46,6 +59,34 @@ pub struct InitializeProgramAuthority<'info> {
     pub system_program: Program<'info, System>,
 }
 
+#[derive(Accounts)]
+pub struct InitializeAuthorityTokenAccount<'info> {
+    #[account(mut)]
+    pub payer: Signer<'info>,
+
+    #[account(
+        constraint = program_authority.is_initialized == true,
+        seeds = [b"signer"],
+        bump = program_authority.bump
+    )]
+    pub program_authority: Account<'info, ProgramAuthority>,
+
+    #[account(
+        init,
+        payer = payer,
+        associated_token::mint = mint,
+        associated_token::authority = program_authority,
+        associated_token::token_program = token_program,
+    )]
+    pub token_account: Account<'info, TokenAccount>,
+
+    pub mint: Account<'info, Mint>,
+
+    pub token_program: Program<'info, Token>,
+    pub associated_token_program: Program<'info, AssociatedToken>,
+    pub system_program: Program<'info, System>,
+}
+
 #[account]
 pub struct ProgramAuthority {
     pub is_initialized: bool,
@@ -54,6 +95,11 @@ pub struct ProgramAuthority {
     // until I figure out how to work with slice types
     pub seeds: String,
     pub bump: u8,
+}
+
+pub enum AssetAccountType {
+    SOL,
+    TOKEN,
 }
 
 impl ProgramAuthority {
--- tests/solana_escrow.ts
+++ tests/solana_escrow.ts
@@ -1,6 +1,12 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { SolanaEscrow } from "../target/types/solana_escrow";
+import {
+  createMint,
+  getAssociatedTokenAddress,
+  ASSOCIATED_TOKEN_PROGRAM_ID,
+  TOKEN_PROGRAM_ID
+} from "@solana/spl-token";
 
 describe("solana_escrow", () => {
   // Configure the client to use the local cluster.
@@ -55,6 +61,62 @@ describe("solana_escrow", () => {
     const buffer = await provider.connection.getAccountInfoAndContext(signer)
 
     console.log(state, buffer)
+
+  });
+
+
+  it("Create Token Account!", async () => {
+
+    const [signer] = anchor.web3.PublicKey.findProgramAddressSync(
+      [Buffer.from("signer")],
+      program.programId
+    )
+
+
+    const mint = await createMint(
+      provider.connection,
+      payer,
+      payer.publicKey,
+      payer.publicKey,
+      9,
+    )
+
+    const tokenAccount = await getAssociatedTokenAddress(
+      mint,
+      signer,
+      true
+    )
+
+
+    const tx = await program.methods
+      .createAuthorityTokenAccount()
+      .accounts({
+
+        payer: payer.publicKey,
+        programAuthority: signer,
+
+        tokenAccount: tokenAccount,
+        mint: mint,
+
+        associatedTokenProgram: ASSOCIATED_TOKEN_PROGRAM_ID,
+        tokenProgram: TOKEN_PROGRAM_ID,
+        systemProgram: anchor.web3.SystemProgram.programId,
+      })
+      .signers([payer])
+      .rpc();
+
+    console.log("Your transaction signature", tx);
+
+    const blockhash = await provider.connection.getLatestBlockhash()
+
+    await provider.connection.confirmTransaction({
+      ...blockhash,
+      signature: tx,
+    }, "confirmed")
+
+    const buffer = await provider.connection.getAccountInfoAndContext(tokenAccount)
+
+    console.log(buffer)
 
   });
 });
