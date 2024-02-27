4d387989b195515962a2a695db18458787d8a1e3
nomadic4life <orbison.dennis@gmail.com>
2024-02-27 03:07:07 -0800

    implemented feature to open a new escrow account, either with sol or a given token mint

--- programs/solana_escrow/src/lib.rs
+++ programs/solana_escrow/src/lib.rs
@@ -1,6 +1,12 @@
 use anchor_lang::prelude::*;
+use anchor_lang::solana_program;
 use anchor_spl::associated_token::AssociatedToken;
-use anchor_spl::token::{Mint, Token, TokenAccount};
+use anchor_spl::token::{transfer, Mint, Token, TokenAccount, Transfer};
+use borsh::{BorshDeserialize, BorshSerialize};
+use core::convert::AsRef;
+// use std::io::Error;
+// use std::io::Read;
+// use std::io::Write;
 
 declare_id!("HNnEWxRvbw5Kf5oCkgTeLa1BNM4racVQELGrEn4K9GQd");
 
@@ -37,6 +43,94 @@ pub mod solana_escrow {
 
         return Ok(());
     }
+
+    pub fn open_escrow_target_sol(
+        ctx: Context<OpenEscrowAccountTargetSol>,
+        data: Params,
+    ) -> Result<()> {
+        let OpenEscrowAccountTargetSol {
+            sender,
+            program_authority,
+            system_program,
+            new_escrow_account,
+        } = ctx.accounts;
+
+        let Params {
+            amount,
+            merkle_root,
+        } = data;
+
+        solana_program::program::invoke(
+            &solana_program::system_instruction::transfer(
+                &sender.key(),
+                &program_authority.key(),
+                amount,
+            ),
+            &[
+                sender.to_account_info(),
+                program_authority.to_account_info(),
+                system_program.to_account_info(),
+            ],
+        )?;
+
+        let clock = Clock::get()?;
+
+        new_escrow_account.initial_date = clock.unix_timestamp;
+        new_escrow_account.sender = sender.key();
+        new_escrow_account.balance = amount;
+        new_escrow_account.merkle_root = merkle_root;
+        new_escrow_account.asset = AssetAccountType::SOL;
+
+        return Ok(());
+    }
+
+    pub fn open_escrow_target_token(
+        ctx: Context<OpenEscrowAccountTargetToken>,
+        data: Params,
+    ) -> Result<()> {
+        let OpenEscrowAccountTargetToken {
+            sender,
+            new_escrow_account,
+            token_program,
+            sender_token,
+            authority_token,
+            mint,
+            ..
+        } = ctx.accounts;
+
+        let Params {
+            amount,
+            merkle_root,
+        } = data;
+
+        transfer(
+            CpiContext::new(
+                token_program.to_account_info(),
+                Transfer {
+                    from: sender_token.to_account_info(),
+                    to: authority_token.to_account_info(),
+                    authority: sender.to_account_info(),
+                },
+            ),
+            amount,
+        )?;
+
+        let clock = Clock::get()?;
+
+        new_escrow_account.initial_date = clock.unix_timestamp;
+        new_escrow_account.sender = sender.key();
+        new_escrow_account.balance = amount;
+        new_escrow_account.merkle_root = merkle_root;
+        new_escrow_account.asset = AssetAccountType::TOKEN { mint: mint.key() };
+
+        return Ok(());
+    }
+}
+
+#[error_code]
+pub enum MyError {
+    #[msg("MyAccount may only hold data below 100")]
+    DataTooLarge,
 }
 
 #[derive(Accounts)]
@@ -78,12 +172,97 @@ pub struct InitializeAuthorityTokenAccount<'info> {
         associated_token::authority = program_authority,
         associated_token::token_program = token_program,
     )]
+    // authority_token
     pub token_account: Account<'info, TokenAccount>,
 
     pub mint: Account<'info, Mint>,
 
     pub token_program: Program<'info, Token>,
     pub associated_token_program: Program<'info, AssociatedToken>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+#[instruction(data: Params)]
+pub struct OpenEscrowAccountTargetSol<'info> {
+    #[account(mut)]
+    pub sender: Signer<'info>,
+
+    #[account(
+        mut,
+        constraint = program_authority.is_initialized == true,
+        seeds = [b"signer"],
+        bump = program_authority.bump
+    )]
+    pub program_authority: Account<'info, ProgramAuthority>,
+
+    #[account(
+        init,
+        payer = sender,
+        space = AssetAccountType::init(0),
+        seeds = [
+            sender.key().as_ref(),
+            data.merkle_root.as_ref(),
+        ],
+        bump
+    )]
+    pub new_escrow_account: Account<'info, EscrowAccount>,
+
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+#[instruction(data: Params)]
+pub struct OpenEscrowAccountTargetToken<'info> {
+    #[account(mut)]
+    pub sender: Signer<'info>,
+
+    #[account(
+        mut,
+        // could use constrait owner is the program id instead of seeds
+        // would it reduce cycle use?
+        constraint = program_authority.is_initialized == true,
+        seeds = [b"signer"],
+        bump = program_authority.bump
+    )]
+    pub program_authority: Account<'info, ProgramAuthority>,
+
+    #[account(
+        mut,
+        constraint = sender_token.owner.key() == sender.key()
+    )]
+    pub sender_token: Account<'info, TokenAccount>,
+
+    #[account(
+        mut,
+        // maybe could just use the owner constraint?
+        constraint = authority_token.owner.key() == program_authority.key(),
+        // seeds = [
+        //     program_authority.key().as_ref(),
+        //     token_program.key().as_ref(),
+        //     mint.key().as_ref(),
+        // ],
+        // bump
+    )]
+    pub authority_token: Account<'info, TokenAccount>,
+
+    #[account(
+        init,
+        payer = sender,
+        space = AssetAccountType::init(1),
+        seeds = [
+            sender.key().as_ref(),
+            data.merkle_root.as_ref(),
+        ],
+        bump
+    )]
+    pub new_escrow_account: Account<'info, EscrowAccount>,
+
+    // could use AccountInfo instead of Account or unchecked
+    pub mint: Account<'info, Mint>,
+
+    pub associated_token_program: Program<'info, AssociatedToken>,
+    pub token_program: Program<'info, Token>,
     pub system_program: Program<'info, System>,
 }
 
@@ -97,11 +276,76 @@ pub struct ProgramAuthority {
     pub bump: u8,
 }
 
+#[account]
+pub struct EscrowAccount {
+    pub initial_date: i64,
+    pub sender: Pubkey,
+    pub merkle_root: Hasher,
+    pub balance: u64,
+    pub asset: AssetAccountType,
+}
+
+#[derive(AnchorSerialize, AnchorDeserialize, Clone)]
 pub enum AssetAccountType {
     SOL,
-    TOKEN,
+    TOKEN { mint: Pubkey },
 }
 
 impl ProgramAuthority {
     const LEN: usize = 8 + 1 + 1 + 6 + 1 + 4;
 }
+
+#[derive(AnchorSerialize, AnchorDeserialize, Clone)]
+pub struct Hasher {
+    pub hash: [u8; 32],
+}
+
+impl AsRef<[u8]> for Hasher {
+    fn as_ref(&self) -> &[u8] {
+        return &self.hash[..];
+    }
+}
+
+impl AssetAccountType {
+    fn init(varient: u8) -> usize {
+        match varient {
+            0 => return 8 + 8 + 32 + 32 + 8 + 1,
+            1 => return 8 + 8 + 32 + 32 + 8 + 1 + 32,
+            _ => return 8 + 8 + 32 + 32 + 8 + 1,
+        }
+    }
+}
+
+#[derive(AnchorSerialize, AnchorDeserialize, Clone)]
+pub struct Params {
+    pub merkle_root: Hasher,
+    pub amount: u64,
+}
+
+// impl AnchorDeserialize for Hash {
+//     fn deserialize_reader<R>(reader: &mut R) -> core::result::Result<Self, Error>
+//     where
+//         R: Read,
+//     {
+//         let buf = &mut [0; 32];
+//         reader.read(buf)?;
+//         return Ok(Hash(hash::Hash::new(buf)));
+//     }
+// }
+
+// impl AnchorSerialize for Hash {
+//     fn serialize<W>(&self, writer: &mut W) -> core::result::Result<(), Error>
+//     where
+//         W: Write,
+//     {
+//         let buf = &[11; 32];
+//         writer.write(buf)?;
+//         return Ok(());
+//     }
+// }
+
+// impl Clone for Hash {
+//     fn clone(&self) -> Self {
+//         return Hash(hash::Hash::new(&[0; 32]));
+//     }
+// }
--- tests/solana_escrow.ts
+++ tests/solana_escrow.ts
@@ -1,33 +1,220 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { SolanaEscrow } from "../target/types/solana_escrow";
+import { createHash } from "crypto";
 import {
   createMint,
+  mintTo,
+
+  getAccount,
+  getMint,
+  createAssociatedTokenAccountInstruction,
+  createMintToInstruction,
+  createInitializeMint2Instruction,
+  createInitializeMintInstruction,
   getAssociatedTokenAddress,
   ASSOCIATED_TOKEN_PROGRAM_ID,
-  TOKEN_PROGRAM_ID
+  TOKEN_PROGRAM_ID,
+  MINT_SIZE,
+  getMinimumBalanceForRentExemptMint
 } from "@solana/spl-token";
 
-describe("solana_escrow", () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+const {
+  sendAndConfirmTransaction,
+  Transaction,
+  LAMPORTS_PER_SOL,
+  SystemProgram,
+} = anchor.web3
 
-  const provider = anchor.getProvider()
-  const program = anchor.workspace.SolanaEscrow as Program<SolanaEscrow>;
-  const payer = anchor.web3.Keypair.generate()
 
-  before(async () => {
-    const tx = await provider.connection.requestAirdrop(payer.publicKey, 100 * anchor.web3.LAMPORTS_PER_SOL)
 
-    const blockhash = await provider.connection.getLatestBlockhash()
+class Token {
+  mint: anchor.web3.Keypair;
+  mintAuthority: anchor.web3.Keypair;   // optional
+  freezeAuthority: anchor.web3.Keypair; // optional
+  supply: number;     // u64
+  decimals: number;   // u8
+  isInitialized: boolean;
 
-    await provider.connection.confirmTransaction({
+  createMint = async (connection: anchor.web3.Connection, payer: anchor.web3.Keypair) => {
+
+    this.mint = anchor.web3.Keypair.generate()
+    this.mintAuthority = anchor.web3.Keypair.generate()
+    this.freezeAuthority = anchor.web3.Keypair.generate()
+    this.supply = 0
+    this.decimals = 9
+    this.isInitialized = false
+
+    const lamports = await getMinimumBalanceForRentExemptMint(connection);
+    const blockhash = await connection.getLatestBlockhash()
+
+    const transaction = new Transaction({ ...blockhash, feePayer: payer.publicKey }).add(
+
+      SystemProgram.createAccount({
+        fromPubkey: payer.publicKey,
+        newAccountPubkey: this.mint.publicKey,
+        space: MINT_SIZE,
+        lamports,
+        programId: TOKEN_PROGRAM_ID,
+      }),
+
+      createInitializeMintInstruction(
+        this.mint.publicKey,
+        this.decimals,
+        this.mintAuthority.publicKey,
+        this.freezeAuthority.publicKey
+      )
+    )
+
+
+    const tx = await sendAndConfirmTransaction(connection, transaction, [payer, this.mint])
+
+    await connection.confirmTransaction({
+      ...blockhash,
+      signature: tx
+    }, "confirmed")
+
+    console.log("TOKEN MINT CREATED")
+  }
+
+
+  airdrop = async (connection, payer, address) => {
+
+    const associatedToken = await getAssociatedTokenAddress(
+      this.mint.publicKey,
+      address || payer.publicKey,
+      true,
+      TOKEN_PROGRAM_ID,
+      ASSOCIATED_TOKEN_PROGRAM_ID
+    )
+
+    const transaction = new Transaction().add(
+      createAssociatedTokenAccountInstruction(
+        address || payer.publicKey,
+        associatedToken,
+        address || payer.publicKey,
+        this.mint.publicKey,
+        TOKEN_PROGRAM_ID,
+        ASSOCIATED_TOKEN_PROGRAM_ID
+      )
+    )
+
+    // seems to be an issue if I use the commitment. but why? is it a bug?
+    // await sendAndConfirmTransaction(connection, transaction, [payer], { commitment: "finalized" })
+    const tx = await sendAndConfirmTransaction(connection, transaction, [payer])
+
+    const blockhash = connection.getLatestBlockhash()
+
+    await connection.confirmTransaction({
+      ...blockhash,
+      signature: tx
+    }, "confirmed")
+
+
+    const transaction2 = new Transaction().add(
+      createMintToInstruction(
+        this.mint.publicKey,
+        associatedToken,
+        this.mintAuthority.publicKey,
+        10 * LAMPORTS_PER_SOL
+      )
+    )
+
+    const tx2 = await sendAndConfirmTransaction(connection, transaction2, [payer, this.mintAuthority])
+
+    const blockhash2 = connection.getLatestBlockhash()
+
+    await connection.confirmTransaction({
+      ...blockhash2,
+      signature: tx2
+    }, "confirmed")
+
+    console.log("AIRDROP SUCCESS")
+
+    return associatedToken
+  }
+}
+
+
+class User {
+
+  keypair: anchor.web3.Keypair | null;
+  address: anchor.web3.PublicKey;
+  associatedTokenAccount: anchor.web3.PublicKey;
+
+  generate = async (connection) => {
+
+    if (!!this.address) {
+      return
+    }
+
+    const payer = anchor.web3.Keypair.generate()
+
+    const tx = await connection.requestAirdrop(payer.publicKey, 1000 * anchor.web3.LAMPORTS_PER_SOL)
+    const blockhash = await connection.getLatestBlockhash()
+
+    await connection.confirmTransaction({
       ...blockhash,
       signature: tx,
     }, "confirmed")
 
-    console.log(await provider.connection.getBalance(payer.publicKey))
-  })
+
+    this.keypair = payer
+    this.address = payer.publicKey
+  }
+
+  airdrop = async (connection, token, payer) => {
+    this.associatedTokenAccount = await token.airdrop(connection, payer, this.address)
+  }
+
+  getPDA = async (connection, program, token, payer) => {
+    const [signer, bump] = anchor.web3.PublicKey.findProgramAddressSync(
+      [Buffer.from("signer")],
+      program.programId
+    )
+
+    if (!!this.address) {
+      const tx = await connection.requestAirdrop(signer, 10000 * anchor.web3.LAMPORTS_PER_SOL)
+
+      const blockhash = await connection.getLatestBlockhash()
+
+      await connection.confirmTransaction({
+        ...blockhash,
+        signature: tx,
+      }, "confirmed")
+
+      this.address = signer
+
+      // await this.airdrop(connection, token, payer)
+
+      console.log(await connection.getBalance(signer))
+    }
+
+
+    return [signer, bump]
+  }
+}
+
+describe("solana_escrow", () => {
+
+  anchor.setProvider(anchor.AnchorProvider.env());
+
+  const provider = anchor.getProvider()
+  const program = anchor.workspace.SolanaEscrow as Program<SolanaEscrow>;
+
+  const token = new Token()
+  const payer = new User()
+  const prog = new User()
+
+  before(async () => {
+
+    await payer.generate(provider.connection)
+    await token.createMint(provider.connection, payer.keypair)
+    await payer.airdrop(provider.connection, token, payer.keypair)
+    await prog.getPDA(provider.connection, program, token, payer.keypair)
+
+    console.log("GENERATED:")
+  });
 
 
   it("Is initialized!", async () => {
@@ -41,11 +228,11 @@ describe("solana_escrow", () => {
     const tx = await program.methods
       .initializeProgramSigner()
       .accounts({
-        payer: payer.publicKey,
+        payer: payer.keypair.publicKey,
         newProgramAuthority: signer,
         systemProgram: anchor.web3.SystemProgram.programId,
       })
-      .signers([payer])
+      .signers([payer.keypair])
       .rpc();
 
     console.log("Your transaction signature", tx);
@@ -72,37 +259,27 @@ describe("solana_escrow", () => {
       program.programId
     )
 
-
-    const mint = await createMint(
-      provider.connection,
-      payer,
-      payer.publicKey,
-      payer.publicKey,
-      9,
-    )
-
     const tokenAccount = await getAssociatedTokenAddress(
-      mint,
+      token.mint.publicKey,
       signer,
       true
     )
-
 
     const tx = await program.methods
       .createAuthorityTokenAccount()
       .accounts({
 
-        payer: payer.publicKey,
+        payer: payer.keypair.publicKey,
         programAuthority: signer,
 
         tokenAccount: tokenAccount,
-        mint: mint,
+        mint: token.mint.publicKey,
 
         associatedTokenProgram: ASSOCIATED_TOKEN_PROGRAM_ID,
         tokenProgram: TOKEN_PROGRAM_ID,
         systemProgram: anchor.web3.SystemProgram.programId,
       })
-      .signers([payer])
+      .signers([payer.keypair])
       .rpc();
 
     console.log("Your transaction signature", tx);
@@ -119,4 +296,131 @@ describe("solana_escrow", () => {
     console.log(buffer)
 
   });
+
+
+  it("Create Escrow Sol Account", async () => {
+
+    const hash = createHash('sha256')
+    const receiver = anchor.web3.Keypair.generate()
+
+    hash.update(receiver.publicKey.toBuffer())
+    const merkleRoot = hash.digest()
+
+    const [escrow] = anchor.web3.PublicKey.findProgramAddressSync(
+      [
+        payer.keypair.publicKey.toBuffer(),
+        merkleRoot
+      ],
+      program.programId
+    )
+
+    const [signer] = anchor.web3.PublicKey.findProgramAddressSync(
+      [Buffer.from("signer")],
+      program.programId
+    )
+
+
+    const tx = await program.methods
+      .openEscrowTargetSol({
+        amount: new anchor.BN(100),
+        merkleRoot: {
+          hash: [...merkleRoot]
+        }
+      })
+      .accounts({
+        sender: payer.keypair.publicKey,
+        programAuthority: signer,
+        newEscrowAccount: escrow,
+        systemProgram: anchor.web3.SystemProgram.programId,
+      })
+      .signers([payer.keypair])
+      .rpc()
+
+    const blockhash = await provider.connection.getLatestBlockhash()
+
+    await provider.connection.confirmTransaction({
+      ...blockhash,
+      signature: tx,
+    }, "confirmed")
+
+    const state = await program.account.escrowAccount.fetch(escrow)
+    const buffer = await provider.connection.getAccountInfoAndContext(escrow)
+
+
+    console.log(buffer.value.data)
+    console.log(state)
+
+
+    console.log(await provider.connection.getBalance(signer))
+  });
+
+
+  it("Create Escrow Token Account", async () => {
+
+    const hash = createHash('sha256')
+    const receiver = anchor.web3.Keypair.generate()
+
+    hash.update(receiver.publicKey.toBuffer())
+    const merkleRoot = hash.digest()
+
+    const [escrow] = anchor.web3.PublicKey.findProgramAddressSync(
+      [
+        payer.keypair.publicKey.toBuffer(),
+        merkleRoot
+      ],
+      program.programId
+    )
+
+    const [signer] = anchor.web3.PublicKey.findProgramAddressSync(
+      [Buffer.from("signer")],
+      program.programId
+    )
+
+    const tokenAccount = await getAssociatedTokenAddress(
+      token.mint.publicKey,
+      signer,
+      true
+    )
+
+
+    const tx = await program.methods
+      .openEscrowTargetToken({
+        amount: new anchor.BN(100),
+        merkleRoot: {
+          hash: [...merkleRoot]
+        }
+      })
+      .accounts({
+        sender: payer.keypair.publicKey,
+        programAuthority: signer,
+        newEscrowAccount: escrow,
+        senderToken: payer.associatedTokenAccount,
+        authorityToken: tokenAccount,
+        mint: token.mint.publicKey,
+        associatedTokenProgram: ASSOCIATED_TOKEN_PROGRAM_ID,
+        tokenProgram: TOKEN_PROGRAM_ID,
+        systemProgram: anchor.web3.SystemProgram.programId,
+      })
+      .signers([payer.keypair])
+      .rpc()
+
+    const blockhash = await provider.connection.getLatestBlockhash()
+
+    await provider.connection.confirmTransaction({
+      ...blockhash,
+      signature: tx,
+    }, "confirmed")
+
+    const state = await program.account.escrowAccount.fetch(escrow)
+    const buffer = await provider.connection.getAccountInfoAndContext(escrow)
+
+
+    console.log(buffer.value.data)
+    console.log(state)
+
+
+    console.log(await provider.connection.getBalance(signer))
+  });
+
+
 });
